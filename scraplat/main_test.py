35a717f5251356d89a2e3f17c82a4f2c63cc099a
unknown <villan188228725@gmail.com>
2016-02-22 16:30:04 +0800

    Nothing changed

--- scraplat/main_test.py
+++ scraplat/main_test.py
@@ -1,8 +1,10 @@
-import manager
+﻿import manager
 if __name__ != "__main__":
     __name__ = "__main__"
 
 
 if __name__ == "__main__":
-    master = manager.manager(domain = "ilazycat.com")
-    master.execute(tasks = ['http://ilazycat.com/'])
+#    master = manager.manager(domain = "ilazycat.com")
+#    master.execute(tasks = ['http://ilazycat.com/'])
+    master = manager.manager(domain = "helloqiu.pw")
+    master.execute(tasks = ['http://blog.helloqiu.pw'])
